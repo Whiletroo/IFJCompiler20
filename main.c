2237e2ea6d25d4a47b3466c353cba0f3d5ecf9e9
Bifriost <64839249+Bifriost@users.noreply.github.com>
2020-10-30 16:41:42 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,144 +3,17 @@
 
 ///DEINITIONS FOR FUNCTIONS
 #define ADD_INST(_inst)														\
-	if (!dynamicStrAddStr(&dyncode, (_inst"\n"))) return false
+	if ((!dynamicStrAddStr(&dyncode,(_inst))) && (!dynamicStrAddStr(&dyncode,("\n")))) return false;
+
 
 #define ADD_CODE(_code)														\
-	if (!dynamicStrAddStr(&dyncode, (_code))) return false
+	if (!dynamicStrAddStr(&dyncode, (_code))) return false;
 
 #define MAX_LENGTH 20
-
-
-
-/// built-in functions
-// Length(s As String) As Integer
-#define FUNCTION_LENGTH														\
-	"\n # Built-in function Length"											\
-	"\n LABEL $length"														\
-	"\n PUSHFRAME"															\
-	"\n DEFVAR LF@%retval"													\
-	"\n STRLEN LF@%retval LF@%0"											\
-	"\n POPFRAME"															\
-	"\n RETURN"
-
-// SubStr(s As String, i As Integer, n As Integer) As String
-#define FUNCTION_SUBSTR														\
-	"\n # Built-in function SubStr"											\
-	"\n LABEL $substr"														\
-	"\n PUSHFRAME"															\
-	"\n DEFVAR LF@%retval"													\
-	"\n MOVE LF@%retval string@"											\
-	"\n DEFVAR LF@length_str"												\
-	"\n CREATEFRAME"														\
-	"\n DEFVAR TF@%0"														\
-	"\n MOVE TF@%0 LF@%0"													\
-	"\n CALL $length"														\
-	"\n MOVE LF@length_str TF@%retval"										\
-	"\n DEFVAR LF@ret_cond"													\
-	"\n LT LF@ret_cond LF@length_str int@0"									\
-	"\n JUMPIFEQ $substr$return LF@ret_cond bool@true"						\
-	"\n EQ LF@ret_cond LF@length_str int@0"									\
-	"\n JUMPIFEQ $substr$return LF@ret_cond bool@true"						\
-	"\n LT LF@ret_cond LF@%1 int@0"											\
-	"\n JUMPIFEQ $substr$return LF@ret_cond bool@true"						\
-	"\n EQ LF@ret_cond LF@%1 int@0"											\
-	"\n JUMPIFEQ $substr$return LF@ret_cond bool@true"						\
-	"\n GT LF@ret_cond LF@%1 LF@length_str"									\
-	"\n JUMPIFEQ $substr$return LF@ret_cond bool@true"						\
-	"\n EQ LF@ret_cond LF@%2 int@0"											\
-	"\n JUMPIFEQ $substr$return LF@ret_cond bool@true"						\
-	"\n DEFVAR LF@max_n"													\
-	"\n MOVE LF@max_n LF@length_str"										\
-	"\n SUB LF@max_n LF@max_n LF@%1"										\
-	"\n ADD LF@max_n LF@max_n int@1"										\
-	"\n DEFVAR LF@edit_n_cond"												\
-	"\n LT LF@edit_n_cond LF@%2 int@0"										\
-	"\n JUMPIFEQ $substr$edit_n LF@edit_n_cond bool@true"					\
-	"\n GT LF@edit_n_cond LF@%2 LF@max_n"									\
-	"\n JUMPIFEQ $substr$edit_n LF@edit_n_cond bool@true"					\
-	"\n JUMP $substr$process"												\
-	"\n LABEL $substr$edit_n"												\
-	"\n MOVE LF@%2 LF@max_n"												\
-	"\n LABEL $substr$process"												\
-	"\n DEFVAR LF@index"													\
-	"\n MOVE LF@index LF@%1"												\
-	"\n SUB LF@index LF@index int@1"										\
-	"\n DEFVAR LF@char"														\
-	"\n DEFVAR LF@process_loop_cond"										\
-	"\n LABEL $substr$process_loop"											\
-	"\n GETCHAR LF@char LF@%0 LF@index"										\
-	"\n CONCAT LF@%retval LF@%retval LF@char"								\
-	"\n ADD LF@index LF@index int@1"										\
-	"\n SUB LF@%2 LF@%2 int@1"												\
-	"\n GT LF@process_loop_cond LF@%2 int@0"								\
-	"\n JUMPIFEQ $substr$process_loop LF@process_loop_cond bool@true"		\
-	"\n LABEL $substr$return"												\
-	"\n POPFRAME"															\
-	"\n RETURN"
-
-// Asc(s As String, i As Integer) As Integer
-#define FUNCTION_ASC														\
-	"\n # Built-in function Asc"											\
-	"\n LABEL $asc"															\
-	"\n PUSHFRAME"															\
-	"\n DEFVAR LF@%retval"													\
-	"\n MOVE LF@%retval int@0"												\
-	"\n DEFVAR LF@cond_length"												\
-	"\n LT LF@cond_length LF@%1 int@1"										\
-	"\n JUMPIFEQ $asc$return LF@cond_length bool@true"						\
-	"\n DEFVAR LF@length_str"												\
-	"\n CREATEFRAME"														\
-	"\n DEFVAR TF@%0"														\
-	"\n MOVE TF@%0 LF@%0"													\
-	"\n CALL $length"														\
-	"\n MOVE LF@length_str TF@%retval"										\
-	"\n GT LF@cond_length LF@%1 LF@length_str"								\
-	"\n JUMPIFEQ $asc$return LF@cond_length bool@true"						\
-	"\n SUB LF@%1 LF@%1 int@1"												\
-	"\n STRI2INT LF@%retval LF@%0 LF@%1"									\
-	"\n LABEL $asc$return"													\
-	"\n POPFRAME"															\
-	"\n RETURN"
-
-// Chr(i As Integer) As String
-#define FUNCTION_CHR														\
-	"\n # Built-in function Chr"											\
-	"\n LABEL $chr"															\
-	"\n PUSHFRAME"															\
-	"\n DEFVAR LF@%retval"													\
-	"\n MOVE LF@%retval string@"											\
-	"\n DEFVAR LF@cond_range"												\
-	"\n LT LF@cond_range LF@%0 int@0"										\
-	"\n JUMPIFEQ $chr$return LF@cond_range bool@true"						\
-	"\n GT LF@cond_range LF@%0 int@255"										\
-	"\n JUMPIFEQ $chr$return LF@cond_range bool@true"						\
-	"\n INT2CHAR LF@%retval LF@%0"											\
-	"\n LABEL $chr$return"													\
-	"\n POPFRAME"															\
-	"\n RETURN"
-
-
-/**
- * Defines built-in functions.
- *
- * @return True if it was successful, false otherwise.
- */
-static bool defBuildFunc()
-{
-	ADD_INST(FUNCTION_LENGTH);
-	ADD_INST(FUNCTION_SUBSTR);
-	ADD_INST(FUNCTION_ASC);
-	ADD_INST(FUNCTION_CHR);
-
-	return true;
-}
 
 bool codeGenStart()
 {
 	if (!dynamicStrInit(&dyncode)) return false;
-
-	if (!defBuildFunc()) return false;
-
 	return true;
 }
 
@@ -148,6 +21,13 @@ void codeGenClear()
 {
 	dynamicStrFree(&dyncode);
 }
+
+bool codeGenOpen()
+{
+	ADD_INST(".IFJcode20");
+	return true;
+}
+
 /***
 * How can I do that function
 */
@@ -156,26 +36,18 @@ bool genArguments(tToken token){
         {
         case TOKEN_INT:
             ADD_CODE("int@");
-            //fprintf(stdout, "int@%i\n", token.attribute.value_int);
             break;
         case TOKEN_DOUBLE:
             ADD_CODE(" double@");
-            int2SAVE_CODE_main_double(token.attribute.value_double);
-            SAVE_CODE_main("\n");
-            //fprintf(stdout, "float@%lf\n", token.attribute.value_double);
             break;
         case TOKEN_STRING:
             ADD_CODE(" string@");
-            SAVE_CODE_main(genWriteString(token));
-            SAVE_CODE_main("\n");
-            //fprintf(stdout, "string@");
-            //genWriteString(token);
-            //fprintf(stdout, "\n");
             break;
             default:
             return  printf("[ERROR] (genArguments) frame input");
             break;
         }
+     return true;
 }
 
 bool genArithm(tToken token, bool stackVersion)
@@ -185,48 +57,38 @@ bool genArithm(tToken token, bool stackVersion)
             switch (token.token_type)
             {
             case TOKEN_PLUS:
-                ADD_INST("ADDS ");
-                //fprintf(stdout, "ADD");
+                ADD_CODE("ADDS ");
                 break;
             case TOKEN_MINUS:
-                ADD_INST("SUBS ");
-                //fprintf(stdout, "SUB");
+                ADD_CODE("SUBS ");
                 break;
             case TOKEN_MUL:
-                ADD_INST("MULS ");
-                //fprintf(stdout, "MUL");
+                ADD_CODE("MULS ");
                 break;
             case TOKEN_DIV:
-                ADD_INST("DIVS ");
-                //fprintf(stdout, "DIV");
+                ADD_CODE("DIVS ");
                 break;
             case TOKEN_HIGHER:
-                ADD_INST("GT ");
-                //fprintf(stdout, "GT");
+                ADD_CODE("GTS ");
                 break;
             case TOKEN_LESS:
-                ADD_INST("LT ");
-                //fprintf(stdout, "LT");
+                ADD_CODE("LTS ");
                 break;
             case TOKEN_EQUALS:
-                ADD_INST("EQS ");
-                //fprintf(stdout, "EQS");
+                ADD_CODE("EQS ");
                 break;
-                /*  We need that part?
+    /*
             case TOKEN_KEYWORD:
                 switch (token.attribute.keyword)
                 {
                 case KEYWORD_AND:
-                    SAVE_CODE_main(" ANDS ");
-                    //fprintf(stdout, " ANDS");
+                    ADD_CODE(" ANDS ");
                     break;
                 case KEYWORD_OR:
-                    SAVE_CODE_main(" ORS ");
-                    //fprintf(stdout, " ORS");
+                    ADD_CODE(" ORS ");
                     break;
                 case KEYWORD_NOT:
-                    SAVE_CODE_main(" NOTS ");
-                    //fprintf(stdout, " NOTS");
+                    ADD_CODE(" NOTS ");
                     break;
                 default:
                     return false;
@@ -242,32 +104,28 @@ bool genArithm(tToken token, bool stackVersion)
             switch (token.token_type)
             {
             case TOKEN_PLUS:
-                ADD_INST("ADD ");
-                //fprintf(stdout, "ADD ");
+                ADD_CODE("ADD ");
                 break;
             case TOKEN_MINUS:
-                ADD_INST("SUB ");
-                //fprintf(stdout, "SUB ");
+                ADD_CODE("SUB ");
                 break;
             case TOKEN_MUL:
-                ADD_INST("MUL ");
-                //fprintf(stdout, "MUL ");
+                ADD_CODE("MUL ");
                 break;
             case TOKEN_DIV:
-                ADD_INST("DIV ");
-                //fprintf(stdout, "DIV ");
+                ADD_CODE("DIV ");
                 break;
             case TOKEN_HIGHER:
-                ADD_INST("GT ");
-                //fprintf(stdout, "GT");
+                ADD_CODE("GT ");
                 break;
             case TOKEN_LESS:
-                ADD_INST("LT ");
-                //fprintf(stdout, "LT");
+                ADD_CODE("LT ");
                 break;
             case TOKEN_EQUALS:
-                ADD_INST("EQ ");
-                //fprintf(stdout, "EQ");
+                ADD_CODE("EQ ");
+                break;
+             case TOKEN_NOT_EQUAL:
+                ADD_CODE("NEQ ");
                 break;
             default:
             return  printf("[ERROR] (genArithm) frame input");
@@ -275,7 +133,257 @@ bool genArithm(tToken token, bool stackVersion)
 
         }
     }
+    return true;
 }
+        //Ф-ция создаст основной фрейм функии
+bool genCreaStartFream(char *nameFrame)
+{
+    if ( strcmp(nameFrame,"main"))
+        {
+        ADD_CODE("LABEL "); ADD_INST("$$main");
+        ADD_INST("CREATEFRAME");
+        ADD_INST("PUSHFRAME");
+        }
+     else {
+	ADD_CODE("LABEL "); ADD_CODE("$"); ADD_CODE(nameFrame);
+	ADD_INST("CREATEFRAME");
+	ADD_INST("PUSHFRAME");
+     }
+	return true;
+}
+    //Функция закроет фрейм
+bool genCreEndFream()
+{
+	ADD_INST("POPFRAME");
+	ADD_INST("CLEARS");
+    ADD_INST("RETURN");
+	return true;
+}
+
+/*           //Нужна ли эта золупа?
+bool genCreateFrame()
+{
+    ADD_INST("CREATEFRAME");
+    return true;
+    //fprintf(stdout, "CREATEFRAME\n");
+}
+*/
+/**
+ * Generates default value of variable.
+ *
+ * @param type Data type of variable.
+ * @return True if it was successful, false otherwise.
+ */
+bool geneVarValue(tToken type, char *valOfType)
+{
+	switch (type.token_type)
+	{
+		case TOKEN_INT:
+			ADD_CODE(" int@");
+            ADD_INST(valOfType);
+			break;
+
+		case TOKEN_DOUBLE:
+			ADD_CODE(" float@");
+			 ADD_INST(valOfType);
+			break;
+
+		case TOKEN_STRING:
+			ADD_CODE(" string@");
+			ADD_INST(valOfType);
+			break;
+
+		case TOKEN_NIL:
+			 ADD_INST(" nil@nil");
+		default:
+			return false;
+	}
+
+	return true;
+}
+
+
+bool genCreVal(char *nameMod)
+{
+    ADD_CODE("DEFVAR "); ADD_CODE("LF@"); ADD_INST(nameMod);
+    return true;
+}
+
+bool geneAssignVal(char *nameMod)
+{
+    ADD_CODE("DEFVAR "); ADD_CODE(" LF@"); ADD_INST(nameMod);
+    ADD_CODE("MOVE "); ADD_CODE(" LF@");
+    return true;
+}
+
+
+bool geneCall(char *nameOfFunc )
+{
+	ADD_CODE("CALL $"); ADD_INST(nameOfFunc);
+	return true;
+}
+
+bool int2Float(char *nameMod,char *retval1,bool stak){
+    if (stak)
+        {
+            ADD_CODE("INT2FLOATS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+            return true;
+        }
+    else
+    {
+       ADD_CODE("INT2FLOATS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+       return true;
+    }
+    return false;
+}
+
+bool int2Char(char *nameMod,char *retval1,bool stak)
+{
+    if (stak)
+        {
+            ADD_CODE("INT2CHARS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+            return true;
+        }
+    else
+    {
+      ADD_CODE("INT2CHAR ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+      return true;
+    }
+    return false;
+}
+
+bool float2Int(char *nameMod,char *retval1,bool stak)
+{
+    if (stak)
+        {
+            ADD_CODE("FLOAT2INTS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+            return true;
+        }
+    else
+    {
+        ADD_CODE("FLOAT2INT ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+        return true;
+    }
+    return false;
+}
+
+bool string2Int(char *nameMod,char *retval1,bool stak)
+{
+    if (stak)
+        {
+            ADD_CODE("STRING2INTS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+            return true;
+
+        }
+    else
+    {
+       ADD_CODE("STRING2INT ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+       return true;
+    }
+    return false;
+}
+
+bool genFunRead(char *nameMod,char *typeVal)
+{
+    ADD_CODE("READ ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" ");ADD_INST(typeVal);
+    return true;
+}
+
+bool genFunWrite(char *nameMod)
+{
+    ADD_CODE("WRITE ");ADD_CODE("LF@");ADD_INST(nameMod);
+    return true;
+}
+
+bool genConCat(char *nameMod,char *retval1)
+{
+    ADD_CODE("CONCAT ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+    return true;
+}
+
+bool genStrLen(char *nameMod)
+{
+    ADD_CODE("STRLEN ");ADD_CODE("LF@");ADD_INST(nameMod);
+    return true;
+}
+
+bool genGetChar(char *nameMod,char *retval1)
+{
+    ADD_CODE("GETCHAR ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE("LF@");ADD_INST(retval1);
+    return true;
+}
+
+bool genSetChar(char *nameMod,char *indFildMod,char *nameChar)
+{
+    ADD_CODE("SETCHAR ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE("int@");
+    ADD_CODE(indFildMod);ADD_CODE("LF@");ADD_INST(nameChar);
+    return true;
+}
+
+bool genType(char *nameMod,char *typeMod,char *nameVarMod)
+{
+    ADD_CODE("TYPE ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" ");
+    ADD_CODE(typeMod);ADD_CODE("@");ADD_INST(nameVarMod);
+    return true;
+}
+
+bool genCreJump(char *nameFunck)
+{
+    ADD_CODE("JUMP ");ADD_CODE(nameFunck);
+    return true;
+}
+
+bool genCreJumpEQ(char *nameFunck, char *nameVarMod, char *typeOfVar, char *valOfVar, bool stak)
+{
+    if (stak)
+    {
+        ADD_CODE("JUMPIFEQS ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
+            ADD_CODE(typeOfVar); ADD_INST(valOfVar);
+            return true;
+    }
+    else
+    {
+        ADD_CODE("JUMPIFEQ ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
+            ADD_CODE(typeOfVar);ADD_INST(valOfVar);
+            return true;
+    }
+    return false;
+}
+
+bool genCreJumpNEQ(char *nameFunck, char *nameVarMod, char *typeOfVar, char *valOfVar, bool stak)
+{
+    if (stak){
+            ADD_CODE("JUMPIFNEQS ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
+                ADD_CODE(typeOfVar);ADD_INST(valOfVar);
+                return true;
+        }
+    else
+    {
+        ADD_CODE("JUMPIFNEQ ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
+                ADD_CODE(typeOfVar);ADD_INST(valOfVar);
+                return true;
+    }
+    return false;
+}
+
+bool genExit(char *val)
+{
+    ADD_CODE("EXIT ");ADD_INST(val);
+    return true;
+}
+
+bool genBreak(char *val)
+{
+    ADD_INST("BREAK");
+    return true;
+}
+
+bool genDpronr(char *valOfvar)
+{
+    ADD_CODE("DPRINT ");ADD_CODE("int@");ADD_INST(valOfvar);
+    return true;
+}
+
 
 
 
