f23842c72894be271a57a7f8ae596da442a2e1f2
Bifriost <64839249+Bifriost@users.noreply.github.com>
2020-11-27 15:15:06 +0100

    Add files via upload

--- gena.c
+++ gena.c
@@ -1,9 +1,9 @@
 #include"gena.h"
-
+#include"symtable.h"
 
 ///DEINITIONS FOR FUNCTIONS
 #define ADD_INST(_inst)														\
-	if ((!dynamicStrAddStr(&dyncode,(_inst))) && (!dynamicStrAddStr(&dyncode,("\n")))) return false;
+	if (!dynamic_string_add_const_str(&code, (_inst "\n"))) return false
 
 
 #define ADD_CODE(_code)														\
@@ -22,78 +22,156 @@ void codeGenClear()
 	dynamicStrFree(&dyncode);
 }
 
+/** Создает Главичку соубору
+ * Generate compulsory sting for correct work
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
 bool codeGenOpen()
 {
 	ADD_INST(".IFJcode20");
+    ADD_INST("JUMP main");
+    ADD_INST(" ");
 	return true;
 }
 
-/***
-* How can I do that function
-*/
-bool genArguments(tToken token){
-    switch (token.token_type)
-        {
-        case TOKEN_INT:
-            ADD_CODE("int@");
-            break;
-        case TOKEN_DOUBLE:
-            ADD_CODE(" double@");
-            break;
-        case TOKEN_STRING:
-            ADD_CODE(" string@");
-            break;
-            default:
-            return  printf("[ERROR] (genArguments) frame input");
-            break;
-        }
-     return true;
+
+
+/** //Ф-ция создаст фрейм функии
+ * Generate main struction of function
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCreaStartFream(char *nameFrame)
+{
+	ADD_CODE("LABEL "); ADD_CODE(nameFrame);
+	ADD_INST("CREATEFRAME");
+
+	return true;
 }
 
-bool genArithm(tToken token, bool stackVersion)
+/**
+ * Generate PUSHFRAME command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCrePushFr() //Ф-ция двинет фрейм
+{
+    ADD_INST("PUSHFRAME");
+    return true;
+}
+
+/**
+ * Generate POPFRAME command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCrePopFr()  //Ф-ция попнет фрейм
+{
+    ADD_INST("POPFRAME");
+    return true;
+}
+
+/** //Ф-ия вызовет фрейм
+ * Generate RETURN command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCreReturn()
+{
+    ADD_INST("RETURN");
+	return true;
+}
+
+
+/** //Ф-ция создаст лабде
+ * Generate LABEL command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCreateLabel(char *Label)
+{
+    ADD_CODE("LABEL ");ADD_INST(Label);
+    return true;
+}
+
+/**
+ * Generate JUMP command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genDestLabelEndJamp(int* Label)
+{
+    ADD_CODE("JUMP ");ADD_INST(Label);
+    return true;
+}
+
+ /** //Ф-ция определит глубину переменной
+ * Check deep of frame
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+void genCheckFrameDeep(int *FramDeep)
+{
+      switch (FrameDeep)
+	{
+		case 1:
+			ADD_CODE(" GF@");
+			break;
+		case 2:
+			ADD_CODE(" LF@");
+			break;
+		case 3:
+			ADD_CODE(" TF@");
+			break;
+		default:
+			return false;
+	}
+}
+
+
+
+ /** //Ф-ция проведет команныу из експрешена
+ * Generates command from expression list
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCheckArithm(tToken token, bool stackVersion,char *result, char *var1,char *var2,TDataType typeValue)
 {
     if (stackVersion)
         {
             switch (token.token_type)
             {
             case TOKEN_PLUS:
-                ADD_CODE("ADDS ");
+                ADD_INST("ADDS ");
                 break;
             case TOKEN_MINUS:
-                ADD_CODE("SUBS ");
+                ADD_INST("SUBS ");
                 break;
             case TOKEN_MUL:
-                ADD_CODE("MULS ");
+                ADD_INST("MULS ");
                 break;
             case TOKEN_DIV:
-                ADD_CODE("DIVS ");
+                ADD_INST("DIVS ");
                 break;
             case TOKEN_HIGHER:
-                ADD_CODE("GTS ");
+                ADD_INST("GTS ");
                 break;
             case TOKEN_LESS:
-                ADD_CODE("LTS ");
+                ADD_INST("LTS ");
                 break;
             case TOKEN_EQUALS:
-                ADD_CODE("EQS ");
+                ADD_INST("EQS ");
                 break;
-    /*
-            case TOKEN_KEYWORD:
-                switch (token.attribute.keyword)
-                {
-                case KEYWORD_AND:
-                    ADD_CODE(" ANDS ");
-                    break;
-                case KEYWORD_OR:
-                    ADD_CODE(" ORS ");
-                    break;
-                case KEYWORD_NOT:
-                    ADD_CODE(" NOTS ");
-                    break;
-                default:
-                    return false;
-                    break;
-                }*/
                 default:
             return  printf("[ERROR] (genArithm) frame input");
             break;
@@ -105,15 +183,44 @@ bool genArithm(tToken token, bool stackVersion)
             {
             case TOKEN_PLUS:
                 ADD_CODE("ADD ");
+                ADD_CODE(result);
+                ADD_CODE(" ");
+                ADD_CODE(var1);
+                ADD_CODE(" ");
+                ADD_INST(var2);
                 break;
             case TOKEN_MINUS:
                 ADD_CODE("SUB ");
+                ADD_CODE(result);
+                ADD_CODE(" ");
+                ADD_CODE(var1);
+                ADD_CODE(" ");
+                ADD_INST(var2);
                 break;
             case TOKEN_MUL:
                 ADD_CODE("MUL ");
+                AADD_CODE(result);
+                ADD_CODE(" ");
+                ADD_CODE(var1);
+                ADD_CODE(" ");
+                ADD_INST(var2);
                 break;
             case TOKEN_DIV:
-                ADD_CODE("DIV ");
+                if (typeValue==FLOAT){
+                    ADD_CODE("DIV ");
+                    ADD_CODE(result);
+                    ADD_CODE(" ");
+                    ADD_CODE(var1);
+                    ADD_CODE(" ");
+                    ADD_INST(var2);
+                }else{
+                    ADD_CODE("IDIV ");
+                    ADD_CODE(result);
+                    ADD_CODE(" ");
+                    ADD_CODE(var1);
+                    ADD_CODE(" ");
+                    ADD_INST(var2);
+                }
                 break;
             case TOKEN_HIGHER:
                 ADD_CODE("GT ");
@@ -135,256 +242,377 @@ bool genArithm(tToken token, bool stackVersion)
     }
     return true;
 }
-        //Г”-Г¶ГЁГї Г±Г®Г§Г¤Г Г±ГІ Г®Г±Г­Г®ГўГ­Г®Г© ГґГ°ГҐГ©Г¬ ГґГіГ­ГЄГЁГЁ
-bool genCreaStartFream(char *nameFrame)
-{
-    if ( strcmp(nameFrame,"main"))
-        {
-        ADD_CODE("LABEL "); ADD_INST("$$main");
-        ADD_INST("CREATEFRAME");
-        ADD_INST("PUSHFRAME");
-        }
-     else {
-	ADD_CODE("LABEL "); ADD_CODE("$"); ADD_CODE(nameFrame);
-	ADD_INST("CREATEFRAME");
-	ADD_INST("PUSHFRAME");
-     }
-	return true;
-}
-    //Г”ГіГ­ГЄГ¶ГЁГї Г§Г ГЄГ°Г®ГҐГІ ГґГ°ГҐГ©Г¬
-bool genCreEndFream()
-{
-	ADD_INST("POPFRAME");
-	ADD_INST("CLEARS");
-    ADD_INST("RETURN");
-	return true;
-}
 
-/*           //ГЌГіГ¦Г­Г  Г«ГЁ ГЅГІГ  Г§Г®Г«ГіГЇГ ?
-bool genCreateFrame()
-{
-    ADD_INST("CREATEFRAME");
-    return true;
-    //fprintf(stdout, "CREATEFRAME\n");
-}
-*/
-/**
+
+/** //Ф-ция определяет тип переменной
  * Generates default value of variable.
  *
- * @param type Data type of variable.
+ *
  * @return True if it was successful, false otherwise.
  */
-bool geneVarValue(tToken type, char *valOfType)
+bool genCheckTypeValue(TDataType type)
 {
-	switch (type.token_type)
+	switch (type)
 	{
-		case TOKEN_INT:
-			ADD_CODE(" int@");
-            ADD_INST(valOfType);
+		case INT:
+			ADD_CODE(" int");
+
 			break;
 
-		case TOKEN_DOUBLE:
-			ADD_CODE(" float@");
-			 ADD_INST(valOfType);
+		case FLOAT:
+			ADD_CODE(" float64");
+
 			break;
 
-		case TOKEN_STRING:
-			ADD_CODE(" string@");
-			ADD_INST(valOfType);
+		case STRING:
+			ADD_CODE(" string");
+
 			break;
 
-		case TOKEN_NIL:
-			 ADD_INST(" nil@nil");
+		case NIL:
+			 ADD_CODE(" nil");
 		default:
 			return false;
 	}
-
 	return true;
 }
 
 
-bool genCreVal(char *nameMod)
+ /** //Ф-ция декларирует переменную
+ * Generate deklaration of variable
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCreDefVar(int *FrameDeep,char *nameMod)
 {
-    ADD_CODE("DEFVAR "); ADD_CODE("LF@"); ADD_INST(nameMod);
+    ADD_CODE("DEFVAR ");
+    genCheckFrameDeep(FrameDeep);
+	ADD_INST(nameMod);
     return true;
 }
 
-bool geneAssignVal(char *nameMod)
+/** //Ф-ция вызовет перемнную
+ * Generate CALL command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool geneCall(char *Label )
 {
-    ADD_CODE("DEFVAR "); ADD_CODE(" LF@"); ADD_INST(nameMod);
-    ADD_CODE("MOVE "); ADD_CODE(" LF@");
-    return true;
-}
-
-
-bool geneCall(char *nameOfFunc )
-{
-	ADD_CODE("CALL $"); ADD_INST(nameOfFunc);
+	ADD_CODE("CALL "); ADD_INST(Label);
 	return true;
 }
 
-bool int2Float(char *nameMod,char *retval1,bool stak){
+/** //Ф-ция вызовет перемнную
+ * Generate READ command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genFunRead(char nameValue,int FrameDeep,TDataType typeValu)
+{
+    ADD_CODE("READ ");
+    genCheckFrameDeep(FrameDeep);
+    ADD_CODE(nameMod);
+    ADD_CODE(" ");
+    genCheckFrameDeep(typeValue);
+
+    return true;
+}
+
+/**
+ * Generate WRITE command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genFunWrite(char *nameMod,int FrameDeep)
+{
+    ADD_CODE("WRITE ");
+    genCheckFrameDeep(FrameDeep);
+    ADD_INST(nameMod);
+    return true;
+}
+
+/**
+ * Generate JUNPIFSQ command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCreJumpEQ(char *Label, char *var1,int *FrameDeep, char *typeOfVar, char *var2, bool stak)
+{
     if (stak)
-        {
-            ADD_CODE("INT2FLOATS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+    {
+        ADD_CODE("JUMPIFEQS ");
+            ADD_CODE(Label);
+            genCheckFrameDeep(FrameDeep);
+            ADD_CODE(var1);
+            ADD_CODE(typeOfVar);
+            ADD_INST(var2);
             return true;
-        }
+    }
     else
     {
-       ADD_CODE("INT2FLOATS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
-       return true;
+        ADD_CODE("JUMPIFEQ ");
+        ADD_CODE(Label);
+        genCheckFrameDeep(FrameDeep);
+        ADD_CODE(var1);
+        ADD_CODE(typeOfVar);
+        ADD_INST(var2);
+        return true;
     }
     return false;
 }
-
-bool int2Char(char *nameMod,char *retval1,bool stak)
+/**
+ * Generate JUNPIFNEQ command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genCreJumpNEQ(char *Label, char *var1,int *FrameDeep, char *typeOfVar, char *var2, bool stak)
 {
-    if (stak)
-        {
-            ADD_CODE("INT2CHARS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+    if (stak){
+            ADD_CODE("JUMPIFEQS ");
+            ADD_CODE(Label);
+            genCheckFrameDeep(FrameDeep);
+            ADD_CODE(var1);
+            ADD_CODE(typeOfVar);
+            ADD_INST(var2);
             return true;
         }
     else
     {
-      ADD_CODE("INT2CHAR ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
-      return true;
-    }
-    return false;
-}
-
-bool float2Int(char *nameMod,char *retval1,bool stak)
-{
-    if (stak)
-        {
-            ADD_CODE("FLOAT2INTS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
-            return true;
-        }
-    else
-    {
-        ADD_CODE("FLOAT2INT ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
+        ADD_CODE("JUMPIFEQ ");
+        ADD_CODE(Label);
+        genCheckFrameDeep(FrameDeep);
+        ADD_CODE(var1);
+        ADD_CODE(typeOfVar);
+        ADD_INST(var2);
         return true;
     }
     return false;
 }
 
-bool string2Int(char *nameMod,char *retval1,bool stak)
-{
-    if (stak)
-        {
-            ADD_CODE("STRING2INTS ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
-            return true;
-
-        }
-    else
-    {
-       ADD_CODE("STRING2INT ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
-       return true;
-    }
-    return false;
-}
-
-bool genFunRead(char *nameMod,char *typeVal)
-{
-    ADD_CODE("READ ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" ");ADD_INST(typeVal);
-    return true;
-}
-
-bool genFunWrite(char *nameMod)
-{
-    ADD_CODE("WRITE ");ADD_CODE("LF@");ADD_INST(nameMod);
-    return true;
-}
-
-bool genConCat(char *nameMod,char *retval1)
-{
-    ADD_CODE("CONCAT ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" LF@");ADD_INST(retval1);
-    return true;
-}
-
-bool genStrLen(char *nameMod)
-{
-    ADD_CODE("STRLEN ");ADD_CODE("LF@");ADD_INST(nameMod);
-    return true;
-}
-
-bool genGetChar(char *nameMod,char *retval1)
-{
-    ADD_CODE("GETCHAR ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE("LF@");ADD_INST(retval1);
-    return true;
-}
-
-bool genSetChar(char *nameMod,char *indFildMod,char *nameChar)
-{
-    ADD_CODE("SETCHAR ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE("int@");
-    ADD_CODE(indFildMod);ADD_CODE("LF@");ADD_INST(nameChar);
-    return true;
-}
-
-bool genType(char *nameMod,char *typeMod,char *nameVarMod)
-{
-    ADD_CODE("TYPE ");ADD_CODE("LF@");ADD_CODE(nameMod);ADD_CODE(" ");
-    ADD_CODE(typeMod);ADD_CODE("@");ADD_INST(nameVarMod);
-    return true;
-}
-
-bool genCreJump(char *nameFunck)
-{
-    ADD_CODE("JUMP ");ADD_CODE(nameFunck);
-    return true;
-}
-
-bool genCreJumpEQ(char *nameFunck, char *nameVarMod, char *typeOfVar, char *valOfVar, bool stak)
-{
-    if (stak)
-    {
-        ADD_CODE("JUMPIFEQS ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
-            ADD_CODE(typeOfVar); ADD_INST(valOfVar);
-            return true;
-    }
-    else
-    {
-        ADD_CODE("JUMPIFEQ ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
-            ADD_CODE(typeOfVar);ADD_INST(valOfVar);
-            return true;
-    }
-    return false;
-}
-
-bool genCreJumpNEQ(char *nameFunck, char *nameVarMod, char *typeOfVar, char *valOfVar, bool stak)
-{
-    if (stak){
-            ADD_CODE("JUMPIFNEQS ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
-                ADD_CODE(typeOfVar);ADD_INST(valOfVar);
-                return true;
-        }
-    else
-    {
-        ADD_CODE("JUMPIFNEQ ");ADD_CODE(nameFunck);ADD_CODE("LF@");ADD_CODE(nameVarMod);
-                ADD_CODE(typeOfVar);ADD_INST(valOfVar);
-                return true;
-    }
-    return false;
-}
-
+/**
+ * Generate EXIT command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
 bool genExit(char *val)
 {
     ADD_CODE("EXIT ");ADD_INST(val);
     return true;
 }
 
-bool genBreak(char *val)
+/**
+ * Generate BREAK command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genBreak()
 {
     ADD_INST("BREAK");
     return true;
 }
 
-bool genDpronr(char *valOfvar)
+/**
+ * Generate DPRINT command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genDpronr(char *valOfvar,TDataType type)
 {
-    ADD_CODE("DPRINT ");ADD_CODE("int@");ADD_INST(valOfvar);
+    ADD_CODE("DPRINT ");
+    genCheckTypeValue(type);
+    ADD_CODE("@");
+    ADD_INST(valOfvar);
     return true;
 }
 
+/**
+ * Generate INT2FLOAT command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool int2Float(char *nameMod,char *retval1,int *FrameDeep,int *FrameDeep1,bool stak){
+    if (stak)
+        {
+            ADD_CODE("INT2FLOATS ");
+            return true;
+        }
+    else
+    {
+            ADD_CODE("INT2FLOATS ");
+        genCheckFrameDeep(FrameDeep);
+            ADD_CODE(nameMod);
+        genCheckFrameDeep(FrameDeep1);
+            ADD_INST(retval1);
+            return true;
+       return true;
+    }
+    return false;
+}
 
+/**
+ * Generate INT2CHAR command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool int2Char(char *nameMod,char *retval1,int *FrameDeep,int *FrameDeep1,bool stak)
+{
+    if (stak)
+        {
+            ADD_CODE("INT2CHARS");
+            return true;
+        }
+    else
+    {
+      ADD_CODE("INT2CHAR ");
+       genCheckFrameDeep(FrameDeep);
+      ADD_CODE(nameMod);
+       genCheckFrameDeep(FrameDeep);
+       ADD_INST(retval1);
+      return true;
+    }
+    return false;
+}
 
+/**
+ * Generate FLOAT2INT command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool float2Int(char *nameMod,char *retval1,int *FrameDeep,int *FrameDeep1,bool stak)
+{
+    if (stak)
+        {
+            ADD_CODE("FLOAT2INTS");
+            return true;
+        }
+    else
+    {
+        ADD_CODE("FLOAT2INT ");
+        genCheckFrameDeep(FrameDeep);
+        ADD_CODE(nameMod);
+        genCheckFrameDeep(FrameDeep1);
+        ADD_INST(retval1);
+        return true;
+    }
+    return false;
+}
 
+/**
+ * Generate STRING2INT command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool string2Int(char *nameMod,char *retval1,int *FrameDeep,int *FrameDeep1,bool stak)
+{
+    if (stak)
+        {
+            ADD_CODE("STRING2INTS");
+            return true;
+        }
+    else
+    {
+       ADD_CODE("STRING2INT ");
+       genCheckFrameDeep(FrameDeep);
+       ADD_CODE(nameMod);
+       genCheckFrameDeep(FrameDeep1);
+       ADD_INST(retval1);
+       return true;
+    }
+    return false;
+}
 
+/**
+ * Generate CONCAT command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genConCat(char *nameMod,char *retval1,int *FrameDeep,int *FrameDeep1,)
+{
+    ADD_CODE("CONCAT ");
+    genCheckFrameDeep(FrameDeep);
+    ADD_CODE(nameMod);
+    genCheckFrameDeep(FrameDeep);
+    ADD_INST(retval1);
+    return true;
+}
+
+/**
+ * Generate STRLEN command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genStrLen(char *nameMod,int *FrameDeep)
+{
+    ADD_CODE("STRLEN ");
+    genCheckFrameDeep(FrameDeep);
+    ADD_INST(nameMod);
+    return true;
+}
+
+/**
+ * Generate STRLEGETCHARN command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genGetChar(char *nameMod,char *retval1,int *FrameDeep,int *FrameDeep1)
+{
+    ADD_CODE("GETCHAR ");
+    genCheckFrameDeep(FrameDeep);
+    ADD_CODE(nameMod);
+    genCheckFrameDeep(FrameDeep);
+    ADD_INST(retval1);
+    return true;
+}
+
+/**
+ * Generate SETCHAR command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genSetChar(char *nameMod,char *indFildMod,char *nameChar,int *FrameDeep,int *FrameDeep1,TDataType type)
+{
+    ADD_CODE("SETCHAR ");
+    genCheckFrameDeep(FrameDeep);
+    ADD_CODE(nameMod);
+    genCheckTypeValue(type);
+    ADD_CODE("@");
+    ADD_CODE(indFildMod);
+    genCheckFrameDeep(type);
+    ADD_INST(nameChar);
+    return true;
+}
+
+/**
+ * Generate SETCHAR command
+ *
+ *
+ * @return True if it was successful, false otherwise.
+ */
+bool genType(char *varToSave,int *FrameDeep,char *typeMod,char *nameOfVar,TDataType type)
+{
+    ADD_CODE("TYPE ");
+    genCheckFrameDeep(FrameDeep);
+    ADD_CODE(varToSave);
+    ADD_CODE(" ");
+    genCheckFrameDeep(type);
+    ADD_CODE("@");
+    ADD_INST(nameVarMod);
+    return true;
+}
